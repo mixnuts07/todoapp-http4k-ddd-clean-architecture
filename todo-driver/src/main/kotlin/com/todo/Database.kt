0a5367baced5bea3abc7f635400d64f277ba49b1
mixnuts07 <french0719@gmail.com>
2024-03-04 08:00:03 +0900

    add ktorm

--- todo-driver/src/main/kotlin/com/todo/Database.kt
+++
@@ -1,5 +0,0 @@
-package com.todo
-
-import org.ktorm.database.Database
-
-val database = Database.connect("jdbc:mysql://localhost:3306/ktorm", user = "root", password = "root")
--- todo-driver/src/main/kotlin/com/todo/TodoDriver.kt
+++ todo-driver/src/main/kotlin/com/todo/TodoDriver.kt
@@ -1,19 +1,32 @@
 package com.todo
 
+import org.ktorm.database.Database
 import org.ktorm.dsl.from
 import org.ktorm.dsl.select
 import org.ktorm.dsl.where
 import org.ktorm.entity.Entity
+import org.ktorm.schema.Table
+import org.ktorm.schema.int
+import org.ktorm.schema.varchar
 
 class TodoDriver {
 
     fun findTodoById(id: String?): Todo {
-        database
-            .from(Todos)
-            .select(Todo.id)
-            .where { (Todo.id = id) }
+        for(row in database.from(Todos).select()) {
+            println(row[Todos.title])
+        }
+        TODO()
     }
 }
+
+val database = Database.connect("jdbc:mysql://localhost:3306/ktorm", user = "root", password = "root")
+
+object Todos: Table<Nothing>("todos") {
+    val id = int("id").primaryKey()
+    val title = varchar("title")
+    val description = varchar("description")
+}
+
 
 interface Todo: Entity<Todo> {
     companion object: Entity.Factory<Todo>()
