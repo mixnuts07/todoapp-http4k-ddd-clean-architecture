5437858c2280ab1cf220095bcf2954be318464a2
mixnuts07 <french0719@gmail.com>
2024-03-06 23:39:29 +0900

    change exposed

--- todo-driver/src/main/kotlin/com/todo/TodoDriver.kt
+++ todo-driver/src/main/kotlin/com/todo/TodoDriver.kt
@@ -1,43 +1,9 @@
 package com.todo
 
-import org.ktorm.database.Database
-import org.ktorm.dsl.*
-import org.ktorm.schema.Table
-import org.ktorm.schema.int
-import org.ktorm.schema.varchar
-
 class TodoDriver {
 
     fun findTodoById(id: String): Todo {
-        val results = database
-            .from(Todos)
-            .select(Todos.title, Todos.description)
-            .where{
-                (Todos.id eq id.toInt())
-            }
-            .map {row ->
-                Todo(
-                    Id(row[Todos.id]),
-                    Title(row[Todos.title]),
-                    Description(row[Todos.description])
-                )
-            }
-        return results[0]
+        TODO()
     }
 }
 
-val database = Database.connect("jdbc:mysql://localhost:3306/ktorm", user = "root", password = "root")
-
-object Todos: Table<Nothing>("todos") {
-    val id = int("id").primaryKey()
-    val title = varchar("title")
-    val description = varchar("description")
-}
-
-
-//interface Todo: Entity<Todo> {
-//    companion object: Entity.Factory<Todo>()
-//    val id: Int
-//    var title: String
-//    var description: String
-//}
